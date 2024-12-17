82c0fe42e0fd577bc4e13b15a2534d11126422ee
Manasrai2000 <manasrai123400@gmail.com>
2024-12-17 09:16:16 +0530

    saving tracker data

--- chat app using socket-io python/server.py
+++ chat app using socket-io python/server.py
@@ -7,11 +7,47 @@ HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
 
+# List of all currently connected client
+active_client = []
+
+def listen_for_message(client, username):
+
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            final_msg = username + ": " + message
+            send_messages_to_all(final_msg)
+
+        else:
+            print("The message send from the client {username} is empty")
+
+
+def send_messages_to_client(client, message):
+    
+    client.sendall(message.encode())
+
+
+# Function to send any new message to all the clients that are currently connected to this server
+def send_messages_to_all(message):
+
+    for user  in active_client:
+        send_messages_to_client(user[1], message)
 
 
 # function to handle client
 def client_handler(client):
     
+    # Server will listen for client message that will contain the username
+    while 1:
+
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_client.append((username,client))
+
+        else:
+            print("Client username is empty")
+
+    threading.Thread(target=listen_for_message)
 
 
 
